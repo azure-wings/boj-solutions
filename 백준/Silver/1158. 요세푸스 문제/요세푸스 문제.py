902ddef4af062d2cc3375d0c4ed75a0222f23d3f
azurewings <70371393+azure-wings@users.noreply.github.com>
2023-01-09 23:12:57 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+import sys
+
+def sol_1158(n: int, k: int) -> List[int]:
+    josephus = []
+    given = list(range(1, n+1))
+    del_idx = k - 1
+    while True:
+        removed = given.pop(del_idx)
+        josephus.append(removed)
+        if len(given):
+            del_idx = (del_idx + k - 1) % len(given)
+        else:
+            break
+    return josephus
+
+n, k = map(int, sys.stdin.readline().split())
+josephus = sol_1158(n, k)
+print(f'<{str(josephus)[1:-1]}>')
