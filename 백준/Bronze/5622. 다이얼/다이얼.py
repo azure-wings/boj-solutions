54838ac6f63cc1b0760a7bb774076119ed0b5647
azurewings <70371393+azure-wings@users.noreply.github.com>
2023-01-10 21:19:51 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,21 @@
+import sys
+
+def sol_5622(s: str) -> int:
+    alphabet_dict = {
+        'A':2, 'B':2, 'C':2,
+        'D':3, 'E':3, 'F':3,
+        'G':4, 'H':4, 'I':4,
+        'J':5, 'K':5, 'L':5,
+        'M':6, 'N':6, 'O':6,
+        'P':7, 'Q':7, 'R':7, 'S':7,
+        'T':8, 'U':8, 'V':8,
+        'W':9, 'X':9, 'Y':9, 'Z':9
+    }
+    time = 0
+    for c in s:
+        time += alphabet_dict[c]+1
+    return time
+
+if __name__ == "__main__":
+    s = sys.stdin.readline().rstrip()
+    print(sol_5622(s))
