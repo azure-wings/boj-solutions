361a0ce7d4a6205ebcf398b19878bbfe42aa525b
azurewings <70371393+azure-wings@users.noreply.github.com>
2023-01-10 14:50:47 +0900

    [Bronze V] Title: 새싹, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+import sys
+
+def sol_25083() -> str:
+    return r'''         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |'''
+
+if __name__ == "__main__":
+    print(sol_25083())
