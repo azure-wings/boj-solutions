85c054b2a363b58f3709bc88f9f55f24e1bef1b9
azurewings <70371393+azure-wings@users.noreply.github.com>
2023-01-10 16:42:43 +0900

    [Bronze V] Title: A+B - 5, Time: 144 ms, Memory: 38640 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -0,0 +1,17 @@
+from typing import List
+import sys
+
+def sol_10950(nums: List[List[int]]) -> List[int]:
+    return map(sum, nums)
+
+if __name__ == "__main__":
+    nums = []
+    while True:
+        inputs = list(map(int, sys.stdin.readline().split()))
+        if inputs != [0, 0]:
+            nums.append(inputs)
+        else:
+            break
+    results = sol_10950(nums)
+    for ret in results:
+        print(ret)
