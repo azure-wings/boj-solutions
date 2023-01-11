28ba24b86072dc416079a94887308d8be0f94e01
azurewings <70371393+azure-wings@users.noreply.github.com>
2023-01-11 15:12:10 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 232 ms, Memory: 118200 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,20 +1,18 @@
 from typing import List
 import sys
 
-def sol_1158(n: int, k: int) -> List[int]:
+def sol_11866(n: int, k: int) -> List[str]:
     josephus = []
-    given = list(range(1, n+1))
-    del_idx = k - 1
-    while True:
-        removed = given.pop(del_idx)
-        josephus.append(removed)
-        if len(given):
-            del_idx = (del_idx + k - 1) % len(given)
-        else:
-            break
+    given = [ i for i in range(1, n+1) ]
+    del_idx = 0
+    while given:
+        del_idx += k - 1
+        if del_idx >= len(given):
+            del_idx %= len(given)
+        josephus.append(str(given.pop(del_idx)))
     return josephus
 
 if __name__ == "__main__":
     n, k = map(int, sys.stdin.readline().split())
-    josephus = sol_1158(n, k)
-    print(f'<{str(josephus)[1:-1]}>')
+    josephus = sol_11866(n, k)
+    print(f'<{", ".join(josephus)}>')
