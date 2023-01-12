19da29cd04ec8fc703bf64b9a37cfce9510cd118
azurewings <70371393+azure-wings@users.noreply.github.com>
2023-01-12 13:28:50 +0900

    [Silver IV] Title: ATM, Time: 156 ms, Memory: 38648 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -3,7 +3,8 @@ from functools import reduce
 import sys
 
 def sol_11399(times: List[int]) -> int:
-    return reduce(lambda acc, x: acc + x[0]*x[1], zip(sorted(times), range(len(times), 0, -1)), 0)
+    n = len(times)
+    return reduce(lambda acc, ix: acc + (n-ix[0])*ix[1], enumerate(sorted(times)), 0)
 
 if __name__ == "__main__":
     _ = sys.stdin.readline()
